f6c05a6a1bd8b3de65bf2909764ccfbb1148e192
amawuaaa <angelurquiola212@hotmail.com>
2025-12-30 22:22:56 +0000

    U4EJ1 reverse array

--- U4/U4EJ1.js
+++ U4/U4EJ1.js
@@ -15,7 +15,9 @@
 console.log(reverseArray(4,7,21,3)); //This will return [3,21,7,4]
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
-
+export function reverseArray(...n){
+  return n.reverse();
+}
 
 
 
